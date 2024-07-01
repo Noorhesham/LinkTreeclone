1c113a6c945fa0ac47038a06925e91a8c903fafe
Noorhesham <noordragon2004@gmail.com>
2024-07-01 20:28:47 +0300

    huge

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import createMiddleware from 'next-intl/middleware'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import createMiddleware from "next-intl/middleware";
 
 const intlMiddleware = createMiddleware({
-  locales: ['en', 'ar'],
-  localePrefix: 'always',
-  defaultLocale: 'en',
-})
+  locales: ["en", "ar"],
+  localePrefix: "always",
+  defaultLocale: "en",
+});
 
-const isPublicRoute = createRouteMatcher(['/', '/ar', '/en'])
+const isPublicRoute = createRouteMatcher(["/", "/ar", "/en",'/sign-in','/en/sign-in','/ar/sign-in','/sign-up','/en/sign-up','/ar/sign-up']);
 
 // const isProtectedRoute = createRouteMatcher(['dashboard/(.*)'])
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect()
+    auth().protect();
   }
 
-  return intlMiddleware(req)
-})
+  return intlMiddleware(req);
+});
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
