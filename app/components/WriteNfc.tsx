72b3388f2b69f7e50f118ce67b3590b75dd8500b
Noorhesham <noordragon2004@gmail.com>
2024-07-10 16:34:32 +0200

    nfc

--- app/components/WriteNfc.tsx
+++ app/components/WriteNfc.tsx
@@ -13,47 +13,48 @@ const NFCWriter: React.FC = () => {
     return `https://yourdomain.com/${username}`;
   };
 
-  const writeToNFC = async (url: string) => {
+  const writeToNFC = async (ndef:any,url: string) => {
     try {
       if ("NDEFWriter" in window) {
-        const ndef = new (window as any).NDEFWriter();
+        
         await ndef.write({ records: [{ recordType: "url", data: url }] });
         setMessage("Successfully written to NFC card!");
       } else {
         setMessage("Web NFC is not supported on this device.");
       }
-    } catch (error:any) {
+    } catch (error: any) {
       console.error("Error writing to NFC card", error);
       setMessage(`Failed to write to NFC card. Error: ${error.message}`);
     }
   };
 
-  const scanNFC = async () => {
+  const scanAndWriteToNFC = async () => {
     try {
       if ("NDEFReader" in window) {
         const ndef = new (window as any).NDEFReader();
         await ndef.scan();
-        ndef.onreading = (event: any) => {
+        ndef.onreading = async (event: any) => {
           setScanningStatus("NFC tag detected");
           const decoder = new TextDecoder();
+          let scannedContent = "";
           for (const record of event.message.records) {
-            setScannedData(decoder.decode(record.data));
+            scannedContent += decoder.decode(record.data);
           }
+          setScannedData(scannedContent);
           setScanningStatus(`NFC tag scanned: ${event.serialNumber}`);
+
+          // Write to the scanned NFC tag
+          const url = generateProfileUrl(username);
+          await writeToNFC(ndef,url);
         };
         setMessage("Scanning for NFC tags...");
       } else {
         setMessage("Web NFC is not supported on this device.");
       }
-    } catch (error:any) {
+    } catch (error: any) {
       console.error("Error scanning NFC card", error);
       setMessage(`Failed to scan NFC card. Error: ${error.message}`);
     }
-  };
-
-  const handleWrite = () => {
-    const url = generateProfileUrl(username);
-    writeToNFC(url);
   };
 
   return (
@@ -61,8 +62,7 @@ const NFCWriter: React.FC = () => {
       <h2>NFC Writer and Scanner</h2>
       <div className="flex flex-col lg:flex-row items-center gap-5">
         <Input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username" />
-        <Button text="Write to NFC" onClick={handleWrite} />
-        <Button text="Scan NFC" onClick={scanNFC} />
+        <Button text="Scan and Write to NFC" onClick={scanAndWriteToNFC} />
       </div>
       {message && <p>{message}</p>}
       {scanningStatus && <p>{scanningStatus}</p>}
