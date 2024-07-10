e6ad9ed622520937954c0b6ec5f6e70316272708
Noorhesham <noordragon2004@gmail.com>
2024-07-10 17:44:00 +0200

    nfc

--- app/[locale]/admin/page.tsx
+++ app/[locale]/admin/page.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import NFCWriter from "../../components/WriteNfc";
 import MaxWidthWrapper from "@/app/components/MaxWidthWrapper";
+import { auth } from "@clerk/nextjs/server";
+import connect from "@/app/lib/db";
+import User from "@/app/lib/models/userModel";
 
-const page = () => {
+const page = async () => {
+  const { userId } = auth();
+  if (userId) {
+    await connect();
+    const user = await User.findOne({ clerkUserId: userId }).lean();
+    //@ts-ignore
+    if (!user.isAdmin) redirect("/");
+  }
   return (
     <MaxWidthWrapper>
       <div className="flex justify-center items-center py-24 lg:py-6 px-12 bg-background  text-xl mt-10 h-full w-full lg:min-h-[60vh]">
