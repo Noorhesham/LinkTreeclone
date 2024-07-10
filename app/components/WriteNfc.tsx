2039a580bf495ff9568df05904b9514bea1a83f2
Noorhesham <noordragon2004@gmail.com>
2024-07-10 17:24:36 +0200

    nfc

--- app/components/WriteNfc.tsx
+++ app/components/WriteNfc.tsx
@@ -9,33 +9,29 @@ const NFCWriter: React.FC = () => {
   const [scannedData, setScannedData] = useState("");
   const [scanningStatus, setScanningStatus] = useState("");
 
-  const generateProfileUrl = (username: string) => {
-    return `https://link-treeclone-olive.vercel.app/${username}`;
-  };
-
   const scanAndWriteToNFC = async () => {
     try {
       if ("NDEFReader" in window) {
+        const urlRecord = {
+          recordType: "url",
+          data: `https://link-treeclone-olive.vercel.app/${username || ""}`,
+        };
+
         const ndef = new (window as any).NDEFReader();
-        await ndef.write("Hello world");
-        // Success! Message has been written.
-        
-        // Now scanning for 3 seconds...
+        await ndef.write({ records: [urlRecord] });
         const abortController = new AbortController();
         await ndef.scan({ signal: abortController.signal });
         const message = await new Promise((resolve) => {
-          ndef.onreading = (event:any) => resolve(event.message);
+          ndef.onreading = (event: any) => resolve(event.message);
         });
-        // Success! Message has been read.
         setMessage(`Message: ${message}`);
         //@ts-ignore
         setScannedData(message);
         await new Promise((r) => setTimeout(r, 3000));
         abortController.abort();
-        };
-        setMessage("Scanning for NFC tags...");
-      } 
-    catch (error: any) {
+      }
+      setMessage("Scanning for NFC tags...");
+    } catch (error: any) {
       console.error("Error scanning NFC card", error);
       setMessage(`Failed to scan NFC card. Error: ${error.message}`);
     }
@@ -45,12 +41,6 @@ const NFCWriter: React.FC = () => {
     <div className="flex flex-col gap-2 h-full w-full lg:w-[60%]">
       <h2>NFC Writer and Scanner</h2>
       <div className="flex flex-col lg:flex-row items-center gap-5">
-        <Input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter username"
-        />
         <Button text="Scan and Write to NFC" onClick={scanAndWriteToNFC} />
       </div>
       {message && <p>{message}</p>}
